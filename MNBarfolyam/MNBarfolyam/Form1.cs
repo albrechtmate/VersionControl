47284e4580a72565caa510dd042d20991d83003d
albrechtmate <albrechtmate@gmail.com>
2020-10-18 15:11:16 +0200

    f4

--- MNBarfolyam/MNBarfolyam/Form1.cs
+++ MNBarfolyam/MNBarfolyam/Form1.cs
@@ -9,23 +9,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Xml;
 
 namespace MNBarfolyam
 {
     public partial class Form1 : Form
     {
         private BindingList<RateData> Rates;
+        public string result;
 
         public Form1()
         {
             InitializeComponent();
 
             elsofuggveny();
-            dataGridView1.DataSource = Rates;
-
+           
+            masodikfuggvenny();
+            harmadikfuggveny();
         }
 
-        private void elsofuggveny()
+        private void harmadikfuggveny()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+        }
+
+        public void elsofuggveny()
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
             var request = new GetExchangeRatesRequestBody()
@@ -35,7 +58,33 @@ namespace MNBarfolyam
                 endDate = "2020-06-30"
             };
             var response = mnbService.GetExchangeRates(request);
-            var result = response.GetExchangeRatesResult;
+            result = response.GetExchangeRatesResult;
+            dataGridView1.DataSource = Rates;
+        }
+        public void masodikfuggvenny()
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+           
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                // Dátum
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                // Valuta
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                // Érték
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
         }
     }
 }
