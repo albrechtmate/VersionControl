f531e6ac71b38b549c80d745e561d6c63ff7007d
albrechtmate <albrechtmate@gmail.com>
2020-10-25 16:41:40 +0100

    2

---
+++ simulation/simulation/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simulation.Entities
+{
+    public class BirthProbability
+    {
+        public int kor { get; set; }
+        public int gyermekSzam { get; set; }
+        public double P { get; set; }
+    }
+}
---
+++ simulation/simulation/Entities/DeathProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simulation.Entities
+{
+    public class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int kor { get; set; }
+        public double P { get; set; }
+
+    }
+}
